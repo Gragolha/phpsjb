8c506e4d600daed9451f2c2ad169b6e5b04f530b
Gragolha <luka.100.nerozi@gmail.com>
2023-08-08 11:26:43 -0300

    novo começo de estudo php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./styles.css" rel="stylesheet" />
     <title>EstudosPHP</title>
 
 </head>
 <body>
-    <?php
-
-
+    <h1> <?php
+    phpinfo();
     ?>
+    </h1>
+
+
+
+
+
+
 
 </body>
 </html>
