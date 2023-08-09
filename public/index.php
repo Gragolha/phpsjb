b1cb7166d916fdc1424684fa54e10c800622b5db
Gragolha <luka.100.nerozi@gmail.com>
2023-08-09 15:52:40 -0300

    fim de turno

--- public/index.php
+++ public/index.php
@@ -1,27 +1,3 @@
-<?php
-session_start();
-
-
-
-if (isset($_POST['acao'])) {
-    echo "form enviado!";
-    $tarefa = strip_tags($_POST['tarefa']);
-
-    if (!isset($_SESSION['tarefa'])) {
-        $_SESSION['tarefas'] = array();
-        $_SESSION['tarefas'][] = $tarefa;
-    } else {
-        $_SESSION['tarefas'][] = $tarefa;
-    }
-}
-?>
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -29,37 +5,16 @@ if (isset($_POST['acao'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listas de tarefas</title>
-    <style type="text/css">
-        /*.dad {
-            display: flex;
-            width: 100%;
-            height: fit-content;
-            margin-top: 20%;
-            flex-direction: column;
-
-
-        }
-
-        * {
-            <?php
-            echo "margin:auto";
-
-
-            ?>
-        }*/
-    </style>
 
 </head>
 
 <body>
-    <div class="dad">
-        <form method="post">
-            <input type="text" name="tarefa" placeholder="Digite sua tarefa" />
-            <input type="submit" name="acao" value="Enviar" />
-        </form>
-        <br />
-        <h3>Sua tarefas</h3>
-    </div>
+    <?php $expression = false; ?>
+    <?php if ($expression == true) : ?>
+        Isso irá aparecer se a expressão for verdadeira.
+    <?php else : ?>
+        Senão isso irá aparecer.
+    <?php endif; ?>
 
 
 </body>
