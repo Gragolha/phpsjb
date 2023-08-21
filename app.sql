417c18c9d2731a5f216032896912f2bdc4c7bc33
Gragolha <luka.100.nerozi@gmail.com>
2023-08-21 11:40:13 -0300

    antes do almoço

---
+++ app.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXIST `NOME_DB`;
+
+USE DATABASE IF EXISTS `NOME_DB`;
+
+CREATE TABLE IF NOT EXISTS `nome_tabela`(
+    uid INTEGER AUTOINCREMENT,
+    username VARCHAR(45) NOT NULL,
+    password VARCHAR(8) NOT NULL,
+    email VARCHAR(90) UNIQUE NOT NULL,
+    PRIMARY KEY(uid),
+);
+
+CREATE TABLE IF NOT EXISTS `nome_tabela2`(
+    postid INTEGER AUTOINCREMENT,
+    uid INTEGER NOT NULL,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    text VARCHAR(255) NOT NULL,
+    PRIMARY KEY (postid),
+    FOREIGN KEY (uid) REFERENCES users(uid),
+)
+
+
+INSERT INTO `nome_tabela` (`username`,`password`, `email`) VALUES (`admin`, `admin`, `admin@example.com`);
+
+SELECT * FROM `nome_tabela`;
+
+SELECT * FROM `nome_tabela` WHERE uid = 1;
+
+SELECT text FROM `nome_tabela2` WHERE uid = 1050;
+
---
+++ doc.md
@@ -0,0 +1,24 @@
+# Estudos
+
+### CRUD
+
+- C = create
+- R = read
+- U = update
+- D = delete
+
+### Create:
+
+    Create = INSERT INTO `nome_tabela`(`props`) VALUES (`valores para as props`);
+
+### Read:
+
+    Read = SELECT * FROM `nome_tabela` WHERE uid = 1;
+
+### Update:
+
+    Update = UPDATE `tabela_nome` SET `username` = `admin1`, `password` = `admin1` WHERE uid = 1;
+
+### Delete:
+
+    Delete = DELETE FROM `tabale_nome` WHERE uid = 1;
